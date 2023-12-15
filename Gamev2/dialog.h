2d7a1b20b4986ee18c3037f5df2c55a5a9f1de55
Unknown12222 <150794604+Unknown12222@users.noreply.github.com>
2023-12-15 19:52:05 +0100

    Add files via upload
    Files for the TP QT

---
+++ Gamev2/dialog.h
@@ -0,0 +1,45 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QWidget>
+#include <QDialog>
+#include <QApplication>
+#include <QtSql/QSqlDatabase>
+#include <QtDebug>
+#include <QtSql/QSqlQuery>
+#include <QTableView>
+#include <QtGui>
+#include <QApplication>
+#include <QSqlTableModel>
+#include <QMessageBox>
+#include <QBoxLayout>
+#include <QPushButton>
+#include <QSqlError>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Dialog;
+}
+QT_END_NAMESPACE
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    Dialog(QWidget *parent = nullptr);
+    ~Dialog();
+public slots:
+    void ShowStats();                   // Création d'une méthode slot en mode public de type void
+
+private slots:
+    void on_QAddnotes_clicked();        // Création d'une méthode slot en mode privé de type void
+
+    void on_Qquit_clicked();
+
+private:
+    Ui::Dialog *ui;
+    QSqlDatabase db;                    // Instantiation d'un objet privé de la classe QSqlDatabase
+    QSqlTableModel *Model;              // Instantiation d'un pointer privé de la classe QSqlTableModel
+};
+#endif // DIALOG_H
